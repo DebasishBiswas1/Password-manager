c7bbfc6584d0030b1d23d4b97507dabb4e779905
DebasishBiswas1 <debasishbiswas.apdj@gmail.com>
2025-06-23 10:46:05 +0530

    Adding encryption to passwords

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+  key = Fernet.generate_key()
+  with open("key.key","wb") as key_file:
+    key_file.write(key)
+
+write_key()
+
+def load_key():
+  file = open('key.key','rb')
+  key = file.read()
+  file.close()
+  return key
+
 master_pwd = input("What is the master password: ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
   with open('passwords.txt','r') as file:
     for line in file.readlines():
       user,passwd = line.rstrip().split(" -> ")
+      print("User:",user," -> Password:",str(fer.decrypt(passwd.encode())))
 
 def add():
   name = input("Account name: ")
   pwd = input("Password: ")
   with open('passwords.txt','a') as file:
-    file.write(name + " -> "+pwd + "\n")
+    file.write(name + " -> "+str(fer.encrypt(pwd.encode())) + "\n")
 
 while True:
   mode = input("Add new password to the list or view passwords ? (Add or View). Press q to quit.").lower()
